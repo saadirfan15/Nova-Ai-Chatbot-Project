0a711d87dc1efd96ad695083b9470a047fcfc6bd
saadirfan15 <isaadirfan697@gmail.com>
2026-07-28 00:20:04 +0500

    Update project files

--- flutter_application_1/lib/config/api_config.dart
+++ flutter_application_1/lib/config/api_config.dart
@@ -2,9 +2,17 @@
 //   static const String baseUrl = "http://127.0.0.1:8000/api";
 //   static const String wsUrl = "ws://127.0.0.1:8000/ws/chat/";
 // }
+// class ApiConfig {
+//   static const String baseUrl = "https://nova-ai-backend-ogch.onrender.com/api";
+
+//   static const String wsUrl =
+//       "wss://nova-ai-backend-ogch.onrender.com/ws/chat/";
+// }
+
 class ApiConfig {
-  static const String baseUrl = "https://nova-ai-backend-ogch.onrender.com/api";
+  static const String baseUrl =
+      "https://nova-ai-chatbot-project-production.up.railway.app/api";
 
   static const String wsUrl =
-      "wss://nova-ai-backend-ogch.onrender.com/ws/chat/";
+      "wss://nova-ai-chatbot-project-production.up.railway.app/ws/chat/";
 }
